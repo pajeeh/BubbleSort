5dba96c3a133650a01c2560bef4a8e84bb771620
Luis Santos <48485199+pajezinho@users.noreply.github.com>
2021-08-19 14:17:22 -0300

    Adicionado a main e a impressão na tela

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,1 +1,17 @@
+import sys
 
+def bolha(indice):
+    n = len(indice)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if indice[j] > indice[j+1]:
+                swap = indice[j]
+                indice[j] = indice[j+1]
+                indice[j+1] = swap
+    return indice
+
+if __name__=='__main__':
+
+    indice = [4,2,18,6,14,10,12,8,16,0,23]
+    print("Indice ordenado: ")
+    print(bolha(indice))
